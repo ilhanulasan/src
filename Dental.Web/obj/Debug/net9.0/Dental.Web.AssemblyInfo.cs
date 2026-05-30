4d191882140dff039551a9b5c42d35db54f3ecba
ilhanulasan <ilhan.ulasan@gmail.com>
2026-05-30 13:11:41 +0300

    Ilk kurulum

--- Dental.Web/obj/Debug/net9.0/Dental.Web.AssemblyInfo.cs
+++ Dental.Web/obj/Debug/net9.0/Dental.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dental.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845fcd3a3175ce3063842262c3bc6e669285b6ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8dcd7ee1413ef46cd9d0ef39adb4970e00d7e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dental.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dental.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
